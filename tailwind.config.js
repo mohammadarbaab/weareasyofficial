3af4f331a69245219533cc5155bc048d2d6df87e
Mohammad Arbaab <kazmiwrite@gmail.com>
2026-01-31 14:45:27 +0530

    add custom announcement

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./layout/*.liquid",
+    "./templates/**/*.liquid",
+    "./sections/**/*.liquid",
+    "./snippets/**/*.liquid"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
